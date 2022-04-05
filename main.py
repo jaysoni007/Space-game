828a24f2406d535b04877f0b151fe611cc2a8e58
Jay Soni <63826001+jaysoni007@users.noreply.github.com>
2022-04-04 23:09:01 -0600

    Create main.py
    Update log 1.1: Created Screen, mainly started learning pygame.

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+
+# Initialize Pygame
+pygame.init()
+
+# Create the Display Screen
+screen = pygame.display.set_mode((800, 600))
+
+# Title and Icon
+pygame.display.set_caption("Asteroids")
+icon = pygame.image.load('asteroid.png')
+pygame.display.set_icon(icon)
+# Game Loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((250, 150, 100)
+    pygame.display.update()
