ceabbf4207355a1dcffa47a68b959d490f3809ab
Jay Soni <63826001+jaysoni007@users.noreply.github.com>
2022-04-08 10:05:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,40 @@ pygame.init()
 # Create the Display Screen
 screen = pygame.display.set_mode((800, 600))
 
+# Create Player
+playerImg = pygame.image.load("playerImg.png")
+playerX = 350
+playerY = 500
+playerX_change = 0
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
 # Title and Icon
 pygame.display.set_caption("Asteroids")
-icon = pygame.image.load('asteroid.png')
+icon = pygame.image.load('ufo (1).png')
 pygame.display.set_icon(icon)
 # Game Loop
 running = True
 while running:
+    # RGB - Red, Green, Blue
+    screen.fill((150, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill((0, 0, 0))
+
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            playerX_change = -0.1
+        if event.key == pygame.K_RIGHT:
+            playerX_change = 0.1
+    if event.type == pygame.KEYUP:
+        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+            playerX_change = 0
+
+    playerX += playerX_change
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >=736:
+        playerX = 736
+    player(playerX, playerY)
     pygame.display.update()
