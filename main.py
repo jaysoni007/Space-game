a18b7b3749bd7929c69b6cd69af71df0a86a8019
Jay Soni <63826001+jaysoni007@users.noreply.github.com>
2022-04-11 23:12:30 -0600

    Updated main.py
    Update V. 1.2: Left - Right Movement Feature, moving enemy; Update V: 1.3 (Ability to destroy enemies with bullets.)

--- main.py
+++ main.py
@@ -1,37 +1,55 @@
 import pygame
-
+import random
 # Initialize Pygame
 pygame.init()
 
 # Create the Display Screen
 screen = pygame.display.set_mode((800, 600))
 
+# Background
+background = pygame.image.load('backgroundImg.png')
+
 # Create Player
 playerImg = pygame.image.load("playerImg.png")
 playerX = 350
 playerY = 500
 playerX_change = 0
+
+# Create Player
+enemyImg = pygame.image.load("enemy.png")
+enemyX = random.randint(0, 800)
+enemyY = random.randint(50, 150)
+enemyX_change = 1
+enemyY_change = 40
+
 def player(x, y):
     screen.blit(playerImg, (x, y))
 
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
+
+
 # Title and Icon
 pygame.display.set_caption("Asteroids")
-icon = pygame.image.load('ufo (1).png')
+icon = pygame.image.load('asteroids.png')
 pygame.display.set_icon(icon)
+
 # Game Loop
 running = True
 while running:
     # RGB - Red, Green, Blue
-    screen.fill((150, 0, 0))
+    screen.fill((0, 0, 0))
+    # Background Img
+    screen.blit(background, (0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_LEFT:
-            playerX_change = -0.1
+            playerX_change = -2.5
         if event.key == pygame.K_RIGHT:
-            playerX_change = 0.1
+            playerX_change = 2.5
     if event.type == pygame.KEYUP:
         if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
             playerX_change = 0
@@ -39,7 +57,19 @@ while running:
     playerX += playerX_change
     if playerX <= 0:
         playerX = 0
-    elif playerX >=736:
+    elif playerX >= 736:
         playerX = 736
+
+    # Enemy Movement
+    enemyX += enemyX_change
+    if enemyX <= 0:
+        enemyX_change = 4
+        enemyY += enemyY_change
+    elif enemyX >= 736:
+        enemyX_change = -4
+        enemyY += enemyY_change
     player(playerX, playerY)
+
+    enemy(enemyX, enemyY)
+
     pygame.display.update()
